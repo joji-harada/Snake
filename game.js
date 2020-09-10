d75ee5ab38ff6e464dc86b1f2d2428b69369ea6e
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-09-10 16:40:25 -0700

    finished snake class

--- game.js
+++ game.js
@@ -100,6 +100,66 @@ class Snake {
     }
 
     move() {
-        
+
+        //movement of tail
+        for (var i = this.tail.length - 1; i > 0; i--) {
+            
+            this.tail[i] = this.tail[i - 1];
+
+        }
+
+        //update start of tail to acquire position of the head
+        if (this.tail.length != 0)
+            this.tail[0] = { x: this.x, y: this.y };
+
+        //movement of head
+        this.x += this.velX * tileSize;
+        this.y += this.velY * tileSize;
+
     }
+
+    //change the direction of movement of snake
+    dir(dirX, dirY) {
+
+        this.velX = dirX;
+        this.velY = dirY;
+
+    }
+
+    //determines whether snake has eaten food
+    eat() {
+        
+        if (Math.abs(this.x - food.x) < tileSize && Math.abs(this.y - food.y) < tileSize) {
+
+            //add to tail
+            this.tail.push({});
+            return true;
+        }
+
+        return false;
+
+    }
+
+    //check for snake death
+    die() {
+
+        for (var i = 0; i < this.tail.length; i++) {
+
+            if (Math.abs(this.x - this.tail[i].x) < tileSize && Math.abs(this.y - this.tail[i].y < tileSize)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    border() {
+
+        if (this.x + tileSize > width && this.velX != -1 || this.x < 0 && this.velX != 1)
+            this.x = width - this.x;
+
+        else if (this.y + tileSize > height && this.velY != -1 || this.velY != 1 && this.y < 0)
+            this.y = height - this.y;
+
+    }
+    
 }
