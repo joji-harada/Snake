5dee75e12725caab9067b3cd4ca2be01bdfe0dc6
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-09-10 16:17:33 -0700

    food class to supply food object

--- game.js
+++ game.js
@@ -3,8 +3,9 @@ let height;
 let titleSize;
 let canvas;
 let ctx;
+let food;
 
-//game objects
+//game objects initialization
 function init() {
 
     titleSize = 20;
@@ -16,5 +17,31 @@ function init() {
     canvas.width = width;
     canvas.height = height;
     ctx = canvas.getContext("2d");
+
+    food = new Food(spawnLocation(), "red");
    
+}
+
+class Food {
+
+    constructor(pos, color) {
+        
+        this.x = pos.x;
+        this.y = pos.y;
+        this.color = color;
+
+    }
+
+    draw() {
+
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, titleSize, titleSize);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 3;
+        ctx.stroke();
+        ctx.closePath();
+
+    }
 }
